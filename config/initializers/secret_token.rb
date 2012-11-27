b00ac8ffa0c8798865a95f6d57b7c2de64678689
Brian Field <brian@madsalty.com>
2012-11-26 21:19:09 -0800

    Modified Gemfile, created barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lacc::Application.config.secret_token = '72eb9092214fc59dd5f98981b1a9ddb2aacfcf9a91a47c7c2aff4e4333b35cddee6658f860dc55d14ab69ca201dd417bdb839eadea46bef35e94de1ca239b094'
